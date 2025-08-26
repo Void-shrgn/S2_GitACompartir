97ebbf4ac07f26f2eb2d292332ae7ccfe9bf66b2
Timi <tommyvtbarros@gmail.com>
2025-08-26 13:26:43 -0400

    Fixes :(

---
+++ Assets/Scripts/TestingScript.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class TestingScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+        Debug.Log("HOLA :)");
+    }
+}
